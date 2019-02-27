4b51a856ffb7f975989b6a251d06da4191970952
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2019-02-27 07:50:53 +0100

    chore(ec): adjust ec config

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
@@ -33,7 +33,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    description="Split multidoc yaml formatted kubernetes descriptors to a set of single resource files",
+    description="Split multidoc yaml formatted kubernetes descriptors to a set \
+        of single resource files",
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + '\n\n' + history,
