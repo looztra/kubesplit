452540a971c7145659ec39286c20b4e8ad549ba7
Christophe Furmaniak <christophe.furmaniak@qima.com>
2019-03-15 17:54:52 +0100

    feat: make kubesplit executable FTW!

--- setup.py
+++ setup.py
@@ -5,43 +5,44 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['ruamel.yaml>0.15']
+requirements = ["ruamel.yaml>0.15"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="Christophe Furmaniak",
-    author_email='christophe.furmaniak@gmail.com',
+    author_email="christophe.furmaniak@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Split multidoc yaml formatted kubernetes descriptors to a set \
         of single resource files",
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='kubesplit',
-    name='kubesplit',
-    packages=find_packages(include=['kubesplit']),
+    keywords="kubesplit",
+    name="kubesplit",
+    packages=find_packages(include=["kubesplit"]),
+    entry_points={"console_scripts": ["kubesplit = kubesplit.__main__:main"]},
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/looztra/kubesplit',
-    version='0.1.0',
+    url="https://github.com/looztra/kubesplit",
+    version="0.1.0",
     zip_safe=False,
 )
