b4bd5025d8640be836a36bc43cf16976e900bc01
Christophe Furmaniak <christophe.furmaniak@decathlon.com>
2025-02-18 19:02:02 +0100

    refactor(ruff): fix RUF012

--- src/kubesplit/k8s_descriptor.py
+++ src/kubesplit/k8s_descriptor.py
@@ -1,6 +1,8 @@
 """Provides a wrapper for a Kubernetes descriptor."""
 
 import os
+from collections.abc import Mapping
+from types import MappingProxyType
 
 
 # pylint: disable=too-many-instance-attributes
@@ -8,30 +10,32 @@ class K8SDescriptor:
     """Kubernetes descriptor."""
 
     _cluster_wide_str_rep = "__clusterwide__"
-    _order_prefixes = {
-        "namespace": "00",
-        "clusterrole": "01",
-        "clusterrolebinding": "02",
-        "serviceaccount": "03",
-        "role": "04",
-        "rolebinding": "05",
-        "secret": "10",
-        "configmap": "11",
-        "persistentvolumeclaim": "12",
-        "persistentvolume": "13",
-        "deployment": "20",
-        "daemonset": "21",
-        "statefulset": "22",
-        "job": "23",
-        "cronjob": "24",
-        "replicaset": "25",
-        "service": "30",
-        "ingress": "31",
-        "networkpolicy": "40",
-        "poddisruptionbudget": "41",
-        "priorityclass": "42",
-        "__unknown__": "99",
-    }
+    _order_prefixes: Mapping[str, str] = MappingProxyType(
+        {
+            "namespace": "00",
+            "clusterrole": "01",
+            "clusterrolebinding": "02",
+            "serviceaccount": "03",
+            "role": "04",
+            "rolebinding": "05",
+            "secret": "10",
+            "configmap": "11",
+            "persistentvolumeclaim": "12",
+            "persistentvolume": "13",
+            "deployment": "20",
+            "daemonset": "21",
+            "statefulset": "22",
+            "job": "23",
+            "cronjob": "24",
+            "replicaset": "25",
+            "service": "30",
+            "ingress": "31",
+            "networkpolicy": "40",
+            "poddisruptionbudget": "41",
+            "priorityclass": "42",
+            "__unknown__": "99",
+        }
+    )
 
     def __init__(  # pylint: disable=too-many-positional-arguments,too-many-arguments
         self,
