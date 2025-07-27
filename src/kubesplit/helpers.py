ff765131482e4ab28dc352b595a8449f25d45abd
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2025-07-27 17:11:17 +0200

    feat(cli): expose yamkix version used (#174)
    * feat(cli): expose yamkix version used
    
    * chore: update pytlint and ruff config
    
    * chore: use mise native config file

--- src/kubesplit/helpers.py
+++ src/kubesplit/helpers.py
@@ -1,11 +1,13 @@
 """Provide generic helpers."""
 
+from yamkix import __version__ as yamkix_version
+
 from kubesplit import __version__
 
 
 def get_version_string() -> str:
     """Return the version string."""
-    return "kubesplit v" + __version__
+    return "kubesplit v" + __version__ + " (yamkix v" + yamkix_version + ")"
 
 
 def print_version() -> None:
